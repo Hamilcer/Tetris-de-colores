e4643564bbceb2fe2e0e3dea809a30f63fec5981
Hamilton Espitia <hamiltonrozo@gmail.com>
2022-02-07 14:51:13 -0500

    finalizado

--- Ficha.java
+++ Ficha.java
@@ -10,7 +10,4 @@ public class Ficha
                               "Amarillo",
                               "Verde   "};
     }
-    // public static String getDefaultColor(){
-    //     return defaultColor;
-    // }
 }
